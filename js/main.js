b1bd2e50cf91a2069e1fae0d904eae70df9c0e13
Arnold Hundert <hunderta@yahoo.com>
2017-01-12 08:53:18 -0500

    Latest updates

--- js/main.js
+++ js/main.js
@@ -32,7 +32,14 @@ window.addEventListener("load",function(){
 		function fadeOutFunc2() {
 			// document.getElementById("startGame").style.opacity = 0;
 			document.getElementById("startGame").style.display = "none";
+		var myVar4;
+		myVar4 = setTimeout(fadeInFunc2, 1000); // 2B used while coding
+		// myVar4 = setTimeout(fadeInFunc2, 4000); // 2B used when coding is done
 		}
+	function fadeInFunc2() {
+		document.getElementById("guessBox").style.opacity = 1;
+	    document.getElementById("guessText").focus();
+	}
 
 		var i;
 		var gameWord;
@@ -41,7 +48,7 @@ window.addEventListener("load",function(){
 		var gameWordCategory;
 		var words = new Array();
 		for(i = 0; i <= 9; i++){
-			words[0] = new Array("Constructor", 11, "The Object of our coding", "WDI");
+			words[0] = new Array("Constructor", 11, "An Object of coding", "WDI");
 			words[1] = new Array("JavaScript", 10, "Coffee writing", "WDI");
 			words[2] = new Array("America", 7, "She's beautiful", "Miscellaneous");
 			words[3] = new Array("Developers", 10, "What we are", "WDI");
@@ -72,32 +79,18 @@ window.addEventListener("load",function(){
 			//	800 - 18 = (782)
 			// wordInfo = 21
 			function createWordDisplay(wordInfo){
-				perCharacter = 782 / parseInt(wordInfo); // 37
-				var wordGuess="";
-				alert("You made it here. " + gameWordLength)
+				var wordArray;
+				var wordGuess;
+				// for (var j = 0; j < wordArray.length; j++) {
+				// 	wordArray[j]
+				// }
+				wordArray = gameWord.split("");
+				wordGuess="";
+				// perCharacter = 782 / parseInt(wordInfo); // 37
 				for(i = 0; i < gameWordLength; i++){
-					wordGuess = wordGuess + "_ ";
+					wordGuess = wordGuess + wordArray[i] + "   ";
 				}
-				document.getElementById("wordArea").innerHTML = wordGuess;
-
-			// totalLength(gameWordLength);
-			// function totalLength(totlen){
-			// 	var x = parseInt(totlen) + (parseInt(totlen) -1);
-			// 	createWordDisplay(x);
-			// }
-
-			// Calculate "spreadout" of word 'dashes'
-			// Word available space
-			//	800 - 18 = (782)
-			// function createWordDisplay(wordInfo){
-			// 	perCharacter = 752 / parseInt(wordInfo);			// 35
-			// 	characterSpace = parseInt(perCharacter) * 2 / 3;	// 23
-			// 	spacesInbetween = parseInt(perCharacter) / 3;		// 11
-			// 	for(i = 0; i <= perCharacter; i = i++){
-			// 		var wordGuess = wordGuess = "_";
-			// 		document.getElementById("wordArea").innerHTML = wordGuess;
-			// 	}
-			// }
+				// document.getElementById("wordArea").innerHTML = wordGuess;
 
 				document.getElementById("hintArea").innerHTML = "Hint: " + gameWordHint;
 			}
--- testtemp.js
+++ testtemp.js
@@ -1,22 +1,90 @@
-		gameWordLength = parseInt(words[i][1]);
-
-
-		totalLength(gameWordLength);
-
-		function totalLength(totlen){
-			var x = parseInt(totlen) + (parseInt(totlen) -1);
-			// x = 21
-			createWordDisplay(x);
+window.addEventListener("load",function(){
+	myFunction();
+	function myFunction() {
+		var myVar;
+		myVar = setTimeout(fadeoutFunc, 1000); // 2B used while coding
+		// myVar = setTimeout(fadeoutFunc, 5000); // 2B used when coding is done
+	}
+	function fadeoutFunc() {
+		document.getElementById("goal").style.opacity = 0;
+		var myVar2;
+		myVar2 = setTimeout(fadeInFunc, 1000); // 2B used while coding
+		// myVar2 = setTimeout(fadeInFunc, 4000); // 2B used when coding is done
+	}
+	function fadeInFunc() {
+		document.getElementById("startGame").style.opacity = 1;
+	}
+	var perCharacter;
+	var characterSpace;
+	var spacesInbetween;
+	document.getElementById("startGame").addEventListener("click", function(){
+		var myVar3;
+		myVar3 = setTimeout(fadeOutFunc2, 1000); // 2B used while coding
+		// myVar3 = setTimeout(fadeOutFunc2, 4000); // 2B used when coding is done
+		function fadeOutFunc2() {
+			// document.getElementById("startGame").style.opacity = 0;
+			document.getElementById("startGame").style.display = "none";
 		}
 
-		// Calculate "spreadout" of word 'dashes'
-		// Word available space
-		//	800 - 18 = (782)
-		// wordInfo = 21
-		var wordGuess;
-		function createWordDisplay(wordInfo){
-			perCharacter = 782 / parseInt(wordInfo);			// 37
-			for(i = 0; i <= wordInfo; i++){
-				wordGuess = wordGuess + "_ ";
-				document.getElementById("wordArea").innerHTML = wordGuess;
+		var i;
+		var gameWord;
+		var gameWordLength;
+		var gameWordHint;
+		var gameWordCategory;
+		var words = new Array();
+		for(i = 0; i <= 9; i++){
+			words[0] = new Array("Constructor", 11, "The Object of our coding", "WDI");
+			words[1] = new Array("JavaScript", 10, "Coffee writing", "WDI");
+			words[2] = new Array("America", 7, "She's beautiful", "Miscellaneous");
+			words[3] = new Array("Developers", 10, "What we are", "WDI");
+			words[4] = new Array("NYCDA", 5, "Where we are", "WDI");
+			words[5] = new Array("Camerican", 9, "Our instructor's github name", "People");
+			words[6] = new Array("Fortune", 7, "Wheel of ?", "Miscellaneous");
+			words[7] = new Array("Independance", 12, "The Declaration of ?", "Miscellaneous");
+			words[8] = new Array("Christmas", 9, "The celebration of Jesus' birth", "Miscellaneous");
+			words[9] = new Array("Jeremy", 6, "NYCDA's 'head honcho'", "People");
+
+			gameWord = words[i][0];
+			gameWordLength = parseInt(words[i][1]);
+			gameWordHint = words[i][2];
+			gameWordCategory = words[i][3];
+
+			document.getElementById("categoryWords").innerHTML = gameWordCategory;
+
+			totalLength(gameWordLength);
+
+			function totalLength(totlen){
+				var x = parseInt(totlen) + (parseInt(totlen) -1);
+				// x = 21
+				createWordDisplay(x);
 			}
+
+			// Calculate "spreadout" of word 'dashes'
+			// Word available space
+			//	800 - 18 = (782)
+			// wordInfo = 21
+			function createWordDisplay(wordInfo){
+				var wordArray = gameWord.split("");
+				for (var j = 0; j < wordArray.length; j++) {
+					// wordArray[j]
+				}
+				perCharacter = 782 / parseInt(wordInfo); // 37
+				var wordGuess="";
+				for(i = 0; i < gameWordLength; i++){
+					wordGuess = wordGuess + wordArray[j] + " ";
+				}
+				alert("gameWordLength = " + gameWordLength + " wordArray.length = " + wordArray.length);
+				document.getElementById("wordArea").innerHTML = wordGuess;
+
+
+				document.getElementById("hintArea").innerHTML = "Hint: " + gameWordHint;
+			}
+
+			// Split word into array
+			function wordTransform(whichWord){
+				// split word
+				var wordTemp = whichWord.split;
+			}
+		}
+	});
+});
