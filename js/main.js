3a25963732165fdae9e0e40ad5f90f438d579033
Arnold Hundert <hunderta@yahoo.com>
2017-01-26 15:51:28 -0500

    Got the 'Correct Guesses' portion working. Next step - handling wrong guesses.

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ window.addEventListener("load",function(){
 	}
 	function fadeInFunc() {
 		document.getElementById("startGame").style.opacity = 1;
+		document.getElementById("startGame").focus();
 	}
 	document.getElementById("startGame").addEventListener("click", function(){
 		var myVar3;
@@ -42,7 +43,7 @@ window.addEventListener("load",function(){
 	});
 
 	var j, k, l;
-	var wordComplete;        // Boolean: Guessing on each word is complete?
+	var wordComplete = false;// Boolean: Guessing on each word is complete?
 	var playersGuess;        // The single character guess that the player makes
 	var gameWord;            // Just what it says - from the array
 	var words = new Array(); // Holds all of the hard-coded words
@@ -57,7 +58,7 @@ window.addEventListener("load",function(){
 		var i;
 		for(i = 0; i <= 9; i++){
 			words[0] = new Array("constructor", 11, "An Object of coding.", "WDI");
-			words[1] = new Array("javaScript", 10, "Coffee writing.", "WDI");
+			words[1] = new Array("javascript", 10, "Coffee writing.", "WDI");
 			words[2] = new Array("television", 10, "The 'boob' box.", "Technology");
 			words[3] = new Array("developers", 10, "What we are now.", "WDI");
 			words[4] = new Array("NYCDA", 5, "Where we learned.", "WDI");
@@ -71,8 +72,10 @@ window.addEventListener("load",function(){
 		myVar6 = setTimeout(wordDisplay, 100);
 	}
 	function wordDisplay() {
-		if (wordComplete == "True"){
+		if (wordComplete == true){
 			whichWord++;
+			wordAreaSpaces = "";
+			wordComplete = false;
 		}
 		gameWord = words[whichWord][0];
 		gameWordLength = parseInt(words[whichWord][1]);
@@ -88,56 +91,69 @@ window.addEventListener("load",function(){
 			wordAreaSpaces = wordAreaSpaces + "_ ";
 			document.getElementById("wordArea").innerHTML = wordAreaSpaces;
 		}
+		document.getElementById("guessText").oninput = function() {myBtnFocus()};
+		function myBtnFocus(){
+			document.getElementById("placeGuest").focus();
+		}
 		document.getElementById("placeGuest").addEventListener("click",playerGuessing);
 	}
 // ********************************************************************************
 	// This function is for the actual player's guessing
-	// wordComplete = "False";
-	// wordComplete = gameWordLength + 8;
-	// while(wordComplete == "False"){
 		function playerGuessing(){
-			playersGuess = document.getElementById("guessText").value;
 			// var wordGuess = "";
 			var wordArray;           // Holds the split string in an array
 			var wordAreaSpacesArray; // To hold updated version of "wordArea"
-			var guessCorrect;        // Boolean to test validity of each guessed letter
-			var wordGuessingAreaSpaces = "";
-
+			var wordGuessingAreaSpaces = ""; // _'s & spaces for word being played
+			var numberRight = 0;     // # of correct letters
+			var underscoreCount = 0; // To determine if word being played is finished
+// ********************************************************************************
+			playersGuess = document.getElementById("guessText").value;
 			//"split" = string --> array
 			wordArray = gameWord.split("");
 			wordGuessingAreaSpaces = document.getElementById("wordArea").innerHTML;
 			wordAreaSpacesArray = wordGuessingAreaSpaces.split("");
-
-			guessRight();
-			function guessRight(){
-				var numberRight = 0;
-				var underscoreCount = 0;
-				for (k = 0; k < gameWordLength; k++) {
-					if (playersGuess == wordArray[k]) {
-						wordAreaSpacesArray[k*2] = playersGuess;
-						numberRight++;
-						guessCorrect = true;
-					}
+// ********************************************************************************
+// ********************************************************************************
+//			           Test player's guess
+			for (k = 0; k < gameWordLength; k++) {
+				if (playersGuess == wordArray[k]) {
+					wordAreaSpacesArray[k*2] = playersGuess;
+					numberRight++; // # of correct letters
 				}
-				if (numberRight == 0) {
-					guessWrong();
-				}
+			}
+// *******************************************************************************
+// 						Testing for the # of correct letters
+			if (numberRight > 0) {
+				guessedRight();
+			// Testing to see if the guessed letter was wrong
+			}else if (numberRight == 0) {
+				guessedWrong();
+			}
+// ********************************************************************************
+			function guessedRight(){
+// 				Testing to see if there are any letters left to be guessed
 				for (l = 0; l < wordAreaSpacesArray.length; l++){
 					if (wordAreaSpacesArray[l] == "_") {
 						underscoreCount++;
 					}
-					if (underscoreCount == 0) {
-						wordComplete = "True";
-						alert("Congratulations! You guessed the word.")
-						wordDisplay()
-					}
 				}
 				document.getElementById("wordArea").innerHTML = wordAreaSpacesArray.join("");
+				if (underscoreCount == 0) {
+					var myVar7;
+					myVar7 = setTimeout(nextWord, 1000);
+				}
+				function nextWord(){
+					wordComplete = true;
+					alert("Congratulations! You guessed the word.");
+					wordDisplay();
+				}
 			}
-			function guessWrong(){
+// ********************************************************************************
+			function guessedWrong(){
 				var remainingGuesses;
 				var remainingGuessesArray;
 				var remainingGuessesValue;
+//				Get the # of remaining guesses from the page
 				remainingGuesses = document.getElementById("remainingGuessesArea").innerHTML;
 				remainingGuessesArray = remainingGuesses.split("");
 
@@ -145,12 +161,11 @@ window.addEventListener("load",function(){
 				remainingGuessesValue = parseInt(x) - 1;
 				document.getElementById("remainingGuessesArea").innerHTML = "Left: " + remainingGuessesValue.toString();
 			}
+// ********************************************************************************
 			document.getElementById("guessText").value = "";
 			document.getElementById("guessText").placeholder="abc";
 			document.getElementById("guessText").focus();
 		}
-// constructor javaScript television developers NYCDA
-// microwave fortune independance Christmas Jeremy
 });
 
 // Plan:
